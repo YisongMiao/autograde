1d74d4faeb6be2e0fad7c10586728fba2cc83b79
Yisong Miao <miaoyisong@gmail.com>
2022-02-20 10:05:45 +0800

    update ta.py, to iterate in python

--- script/ta.py
+++ script/ta.py
@@ -1,4 +1,6 @@
+import re
 import sys
+import os
 
 # from A0000001L import Tokenizer
 
@@ -15,7 +17,19 @@ def importName(modulename, name):
 if __name__ == '__main__':
     # Yisong: Yuxi, you can change here into a param, Q1/Q2/Q3/Q4
     sys.path.insert(1, '../data/Q1')
-    # Yisong: Yuxi, you can also parameterize it: A0000001L, A0000002L ...
-    Tokenizer = importName('A0000001L', "Tokenizer")
-    print(Tokenizer)
-    print('Successfully import a class from a student')
+
+    rootDir = '../data/Q1'
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        if re.search(r'pycache', dirName): continue  # handle possible pycache issue
+        print('Found directory: {}'.format(dirName))
+        print('Found {} file: {}'.format(len(fileList), fileList))
+        for fname in fileList:
+            # Yisong: Yuxi, you can also parameterize it: A0000001L, A0000002L ...
+            print('-----Executing a new file-----\nCurrent file name is: {}'.format(fname))
+            if not re.search(r'A[0-9]+[A-Z].py', fname):
+                print('Not correct file')
+                continue
+            matric_num = re.findall(r'A[0-9]+[A-Z]', fname)[0]
+            Tokenizer = importName(matric_num, "Tokenizer")
+            print(Tokenizer)
+            print('Successfully import a class from a student')
